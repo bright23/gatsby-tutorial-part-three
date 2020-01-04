a2be6be5060f8724137dc88f7d2bbe1ea860db49
WataruFujiwara <wataru930@gmail.com>
2020-01-04 21:49:38 +0900

    change : about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,7 +2,9 @@ import React from "react"
 
 export default () => (
   <div>
-    <h1>About me</h1>
-    <p>I’m good enough, I’m smart enough, and gosh darn it, people like me!</p>
+    <h1>I'd love to talk! Email me at the address below</h1>
+    <p>
+      <a href="mailto:me@example.com">me@example.com</a>
+    </p>
   </div>
 )
